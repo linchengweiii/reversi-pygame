719c980fc67a359479fec32c0aed19596ab2a6a1
Lin Cheng Wei <snoopy90338@gmail.com>
2020-11-25 15:01:53 +0800

    clean code for arena

--- arena.py
+++ arena.py
@@ -8,58 +8,57 @@ import importlib
 import argparse
 from tqdm.auto import tqdm
 
-def run_agent(agent: BaseAgent, reward: dict, obs: dict):
-    action, event_type = agent.step(reward, obs)
-    reward = play_ground.act(action, event_type) # reward after an action
-    return reward
+class Arena(object):
+    def __init__(self, playground, player1, player2):
+        self.playground = playground
+        self.player1 = player1
+        self.player2 = player2
 
-def main(play_ground, agent1, agent2, rounds):
-    # start our loop
-    n_black_wins = 0
-    for i in tqdm(range(rounds)):
-        reward1, reward2 = {}, {}
-        if play_ground.game_over():
-            play_ground.reset_game()
+    def run(self, n_rounds):
+        n_rounds_black_wins = 0
+        for i in tqdm(range(n_rounds)):
+            reward1, reward2 = {}, {}
+            if self.playground.game_over():
+                self.playground.reset_game()
 
-        run_iter = 0
-        while play_ground.game_over() == False:
-            if (run_iter % 2 == 0):
-                obs = play_ground.get_game_state()
-                while True:
-                    try:
-                        reward1 = run_agent(agent1, reward1, obs)
-                        if reward1[-1] != 0:
-                            break
-                    except (utils.ValueOutOfRange, utils.InvalidAction) as e:
-                        # print("invalid action! retry!")
-                        pass
-                    except utils.NoAvailableAction:
-                        # print("ignore black action")
-                        run_iter += 1
-                        break
-            else:
-                obs = play_ground.get_game_state()
-                while True:
-                    try:
-                        reward2 = run_agent(agent2, reward2, obs)
-                        if reward2[1] != 0:
-                            break
-                    except (utils.ValueOutOfRange, utils.InvalidAction) as e:
-                        # print("invalid action! retry!") 
-                        pass
-                    except utils.NoAvailableAction:
-                        # print("ignore white action")
-                        run_iter += 1
-                        break
-            run_iter += 1 
-        
-        play_ground._draw_frame()
+            it = 0
+            while playground.game_over() == False:
+                if it % 2 == 0:
+                    update_iter, reward1 = self.step(-1, self.player1, reward1)
+                    it += update_iter
+                else:
+                    update_iter, reward2 = self.step(1, self.player2, reward2)
+                    it += update_iter
+            
+            self.playground._draw_frame()
+
+            if self.playground.game.winner == -1:
+                n_rounds_black_wins += 1
+
+        print ('Your win rate is', n_rounds_black_wins / n_round * 100, '%', end='\r')
 
 
-        if game.winner == -1:
-            n_black_wins += 1
+    def run_agent(self, agent, reward, obs):
+        action, event_type = agent.step(reward, obs)
+        reward = self.playground.act(action, event_type) # reward after an action
+        return reward
 
-    print ('Your win rate is', n_black_wins / rounds)
+    def step(self, player_index, agent, reward):
+        obs = self.playground.get_game_state()
+        while True:
+            try:
+                reward = self.run_agent(agent, reward, obs)
+                if reward[player_index] != 0:
+                    return 1, reward
+
+            except (utils.ValueOutOfRange, utils.InvalidAction) as e:
+                # print("invalid action! retry!")
+                pass
+
+            except utils.NoAvailableAction:
+                # print("ignore black action")
+                return 2, reward
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -67,7 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('--agent2', default="base_agent.RandomAgent")
     parser.add_argument('--width', default=600, type=int)
     parser.add_argument('--height', default=600, type=int)
-    parser.add_argument('--rounds', default=10, type=int)
+    parser.add_argument('--n_rounds', default=10, type=int)
     parser.add_argument('--time_limit', default=30000, type=int)
     parser.add_argument('--headless', action='store_true')
     args = parser.parse_args()
@@ -78,17 +77,19 @@ if __name__ == "__main__":
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
     game = Reversi(width = args.width, height = args.height, time_limit=args.time_limit)
-    play_ground = Environment(game, force_fps = False)
+    playground = Environment(game, force_fps = False)
     rev_board = ReversiBoard()
 
     # init agent and game.
-    play_ground.init()
-    play_ground.display_screen = True
+    playground.init()
+    playground.display_screen = True
 
     agent1_module = importlib.import_module("agent."+args.agent1.split('.')[0])
     agent2_module = importlib.import_module("agent."+args.agent2.split('.')[0])
 
     agent1 = getattr(agent1_module, args.agent1.split('.')[1])(color = "black", rows_n = len(rev_board.rows), cols_n = len(rev_board.cols), width = args.width, height = args.height)
     agent2 = getattr(agent2_module, args.agent2.split('.')[1])(color = "white", rows_n = len(rev_board.rows), cols_n = len(rev_board.cols), width = args.width, height = args.height)
-    main(play_ground, agent1, agent2, args.rounds)
+
+    arena = Arena(playground, agent1, agent2)
+    arena.run(args.n_rounds)
 
